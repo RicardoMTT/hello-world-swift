9b56c67a3b76faf6579dddbdd736042fba6404b5
Ricardo Tovar <tricardo003@gmail.com>
2023-11-14 16:44:22 -0500

    Initial Commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Ricardo Manuel Tovar Taboada on 14/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
